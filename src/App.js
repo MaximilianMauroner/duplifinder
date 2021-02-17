f7849d545af171991af6a046b7ece6f51e7f9027
Maximilian Mauroner <maximilian.mauroner@gmail.com>
2021-02-17 13:41:40 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import CSVReader from 'react-csv-reader'
+import DisplayDouplicatePW from "./DisplayDouplicatePW";
+import Button from '@material-ui/core/Button';
+import FilterCenterFocusIcon from '@material-ui/icons/FilterCenterFocus';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './fileuploader.css'
+
+
+class App extends Component {
+    state = {
+        doubles: [],
+        loadedData: false,
+        file: "Select your file!(LastPass only)"
+    }
+
+    componentDidMount() {
+    }
+
+    pwIsInDoubles = (doubles, data) => {
+
+        for (let i = 0; i < doubles.length; i++) {
+            // if (data[0] === "http://sn" || data[0] === "http://") {
+            //     return doubles;
+            // }
+
+            if (data[2] && doubles[i][0][2] === data[2]) {
+
+                let temp = doubles[i]
+                doubles.splice(i, 1);
+                temp.push(data)
+                doubles.push(temp)
+                return doubles;
+            }
+        }
+        let temp = []
+        temp.push(data)
+        doubles.push(temp)
+        return doubles;
+    }
+
+    getDoubles = (doubles) => {
+        let samePW = []
+        for (let i = 0; i < doubles.length; i++) {
+            if (doubles[i].length > 1) {
+                samePW.push(doubles[i])
+            }
+        }
+        samePW.sort((a, b) => a.length - b.length)
+
+
+        this.setState({
+            doubles: samePW,
+            loadedData: true
+        })
+    }
+
+    checkDoubles = (data, fileInfo) => {
+        this.setState({
+            file: fileInfo.name,
+            loadedData: false,
+            doubles: []
+        })
+        this.setState({disabled: true})
+        let pwdata = []
+        for (let i = 1; i < data.length; i++) {
+            pwdata = this.pwIsInDoubles(pwdata, data[i])
+        }
+        this.getDoubles(pwdata)
+    }
+
+
+    resetData = () => {
+        this.setState({
+            doubles: [],
+            loadedData: false,
+            file: "Select your file!(LastPass only)"
+        })
+    }
+
+
+    render() {
+        if (this.state.loadedData) {
+            return (
+                <div className={"justify-content-md-center"}>
+                    <div className="container">
+                        <div className={"w-50 justify-content-center"}>
+                            <DisplayDouplicatePW data={this.state.doubles}/>
+                        </div>
+                    </div>
+                    <div>
+                        <Button className={"again-upload"} onClick={this.resetData}/>
+                    </div>
+                </div>
+            )
+        }
+        return (
+            <div>
+                <div className="container">
+                    <form className="form">
+                        <div className={"file-upload-wrapper"} data-text={this.state.file}>
+                            <CSVReader onFileLoaded={(data, fileInfo) => this.checkDoubles(data, fileInfo)}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/DisplayDouplicatePW.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {Row, Card, Col} from "react-bootstrap";
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import './displayDouplicates.css'
+
+
+const DisplayDouplicatePW = ({data}) => {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+    let ret = []
+
+    for (let i = 0; i < data.length; i++) {
+        let temp = []
+        for (let j = 0; j < data[i].length; j++) {
+            temp.push(
+                <Row>
+                    <Col className={"card-text"}>
+                        <Card.Title>
+                            <b>Name:</b> {data[i][j][4]}
+                        </Card.Title>
+                        <Card.Subtitle>
+                            <b>username: </b>{data[i][j][1]}
+                        </Card.Subtitle>
+                        <Card.Link>
+                            <b>URL: </b> {data[i][j][0]} <a target="_blank" href={data[i][j][0]}> click</a>
+                        </Card.Link>
+                    </Col>
+                </Row>
+            )
+
+            if (j < data[i].length - 1) {
+                temp.push(<hr className="solid"/>)
+            }
+
+
+        }
+        ret.push(
+            <Accordion expanded={expanded === i} onChange={handleChange(i)}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls={"panel" + i + "bh-content"}
+                    id={i}
+                >
+                    <Typography className={"heading"}>Same Passwords</Typography>
+                    <Typography className={"secondaryHeading"}>{data[i].length}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div>
+                        {temp}
+                    </div>
+                </AccordionDetails>
+            </Accordion>
+        )
+    }
+    return (
+        <div className={"loadcontainer"}>
+            {ret}
+        </div>
+    );
+}
+
+
+export default DisplayDouplicatePW
